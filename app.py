d75f3c7b840e52d4e97f55623fb28cf196511f46
andra-ganteng <arzilito@gmail.com>
2026-06-22 00:30:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ app = Flask(__name__)
 import os
 
 app.config['MYSQL_HOST'] = os.getenv('MYSQLHOST')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQLPORT') or 3306)
 app.config['MYSQL_USER'] = os.getenv('MYSQLUSER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQLPASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQLDATABASE')
-mysql_port = os.getenv('MYSQLPORT')
 
 if not mysql_port:
     mysql_port = 3306
