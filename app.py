a3ef8287312a3bfc81143aaaeed9fa2e86a824fb
andra-ganteng <arzilito@gmail.com>
2026-06-22 00:13:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.config['MYSQL_HOST'] = os.getenv('MYSQLHOST')
 app.config['MYSQL_USER'] = os.getenv('MYSQLUSER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQLPASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQLDATABASE')
-app.config['MYSQL_PORT'] = int(os.getenv('MYSQLPORT'))
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQLPORT', '3306'))
 
 mysql = MySQL(app)
 print("HOST:", os.getenv('MYSQLHOST'))
